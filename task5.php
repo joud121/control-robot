cc95266c28bae9438eeecb95a8ee4a4592c92d6d
joud121 <74363718+joud121@users.noreply.github.com>
2022-08-23 00:25:53 +0300

    Update task5.php

--- task5.php
+++ task5.php
@@ -3,34 +3,7 @@
 /*1- first: on in your web site page phpmyadmin.com .
 2-seconde  enter (NEW ) TO DO DATABASE NAME IS "phptest" NEKEST   name table IS "contacts".
 3-  CREATE THREE column (ID ,NUME,NUM) "ID": IS INT PRUMARY KEY -- "NUME": IS VARCHARE  -- "NUM": IS INT  */
-/*
-$data=$_GET["x"];
 
-$host = "localhost";
-$username = "root";
-$password = "";
-
-try 
-{
-    $conn = new PDO("mysql:host=$host;dbname=dir_db", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage();
-}
-
-if(isset($_GET['save_contact']))
-{	//print_r($_GET);
-	
-	
-	$sql = "INSERT INTO dir(id,direction) VALUES( '','".addslashes($data)."')";
-	if($conn->query($sql)==TRUE){
-	echo"successfully";}
-	else{ echo"error :" . $sql . "<br>" .$conn->error; }
-	$conn->close();
-
-}*/
 $data=$_GET["x"];
 $servername = "localhost";
 $username = "root";
