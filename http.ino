0308e099b5d136b9569463353fa5df0e2831c00f
joud121 <74363718+joud121@users.noreply.github.com>
2022-08-24 09:02:57 +0300

    Add files via upload

---
+++ http.ino
@@ -0,0 +1,34 @@
+#include <HTTPClient.h>
+#include <wifi.h>
+
+const char* ssid = "Vwoki-GUEST";
+const char* password = "";
+
+
+const string url = "https://s-m.com.sa/r2/test/cpanel/retrieve.php"; // or change to your PC's IP address "https://192.168.56.1/sensors/select.php"
+
+
+void setup() {
+
+Serial.begin(115200);
+WiFi.begin(ssid,password);
+
+serial.print("connecting to WiFi);
+while(WiFi.status()!= WL_CONNECTED){
+dalay(500);
+serial.print(".");
+}
+
+}
+
+void loop() {
+HTTPClient http;
+http.begin(url);
+
+int httpResponseCode = http.GET();
+
+string payload =http.getstring();
+  Serial.println();
+  Serial.println(payload);
+
+}
